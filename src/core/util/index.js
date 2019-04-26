d69820824256951d3dfb62accc759895cb2967a7
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2019-04-26 11:22:31 +0800

    Update index.js

--- src/core/util/index.js
+++ src/core/util/index.js
@@ -1,8 +1,12 @@
 import render from "../../utils/render";
 import create from "../../utils/create";
+import random from "../../utils/random";
+import capitalize from "../../utils/capitalize";
 
 
 export default create({
   create,
-  render
+  render,
+  random,
+  capitalize
 });
